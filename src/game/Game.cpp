02191c2bcdf055e9f29431457a62999068a682f1
kristian <kristian.dam.pedersen@gmail.com>
2023-06-18 17:48:59 +0200

    small steps in a working direction

---
+++ src/game/Game.cpp
@@ -0,0 +1,16 @@
+#include "Game.h"
+
+Game::Game() {}
+
+Game::~Game() {}
+
+void Game::handleEvents() {}
+
+void Game::update() {}
+
+void Game::render() {}
+
+void Game::clean() {}
+
+bool Game::running() {}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "SDL.h"
+#include "SDL_image.h"
 
 int main() {
     SDL_Log("Hello World!");
